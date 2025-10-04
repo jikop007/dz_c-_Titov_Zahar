ca449cb731d21f63e57ff4ddfc13ade2bd4d970a
Titov Zakhar <jikop007@gmail.com>
2025-10-04 17:46:06 +0300

    exercise 3, in exercise 1 fixed codestyle

--- n1.cs
+++ n1.cs
@@ -4,14 +4,14 @@ class Program1
 {
     static void Main()
     {
-        int DegreesСelsius = 0;
+        int degreesСelsius = 0;
         Console.Write("Введите температуру в градусах Цельсия: ");
         string? input = Console.ReadLine();
 
-        if (input != null && int.TryParse(input, out DegreesСelsius))
+        if (input != null && int.TryParse(input, out degreesСelsius))
         {
             Console.Write("Температура в Форенгейтах: ");
-            Console.Write(DegreesСelsius * 9 / 5 + 32);
+            Console.Write(degreesСelsius * 9 / 5 + 32);
         }
         else
         {
